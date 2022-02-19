160518b960a27e0e7f37c9575b27f16b2a067196
kripton1 <bodya7613@gmail.com>
2022-02-19 17:46:59 +0200

    Added joining and exiting from event

--- routes/events.js
+++ routes/events.js
@@ -4,9 +4,7 @@ const path = require("path");
 const controller = require('../controllers/events');
 
 router.get('/all', controller.all);
-router.get('/:id', controller.byId);
 router.get('/create', controller.create);
-//router.get('/join/:id', controller.join);
-//router.get('/delete/:id', controller.delete);
+router.get('/:id', controller.byId);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -19,18 +19,29 @@ module.exports = (io) => {
       }
 
       db.get(
-        "SELECT * FROM ev_users WHERE email = ? AND password = ?",
+        "SELECT * FROM ev_users WHERE email = ?",
         obj.email,
-        crypto.encrypt(obj.password),
         (err, row) => {
           if (err) throw err;
           if (!row) io.emit("user.login.error", "Invalid email or password");
           else {
-            delete row.password;
-            delete row.date_reg;
-            delete row.date_lastlogin;
+            if (crypto.decrypt(row.password)+'' !== obj.password+'') io.emit("user.login.error", "Invalid email or password");
+            else{
+              delete row.password;
+              delete row.date_reg;
+              delete row.date_lastlogin;
 
-            io.emit("user.login.success", crypto.encrypt(JSON.stringify(row)));
+              db.run(
+                "UPDATE ev_users SET date_lastlogin = ? WHERE id = ?",
+                new Date().getTime(),
+                row.id,
+                (err) => {
+                  if (err) throw err;
+                  else io.emit("user.login.success", crypto.encrypt(JSON.stringify(row)));
+                }
+              );
+            }
+            
           }
         }
       );
@@ -102,33 +113,43 @@ module.exports = (io) => {
       );
     });
 
-
-
-    socket.on('events.get.list', (token)=>{
-      db.all('SELECT * FROM ev_events', async (err, rows)=>{
-        if (rows.length){
+    socket.on("events.get.list", (token) => {
+      db.all("SELECT * FROM ev_events", async (err, rows) => {
+        if (rows.length) {
           for (const row of rows) {
-            const admin = await db.queryOne('SELECT id, name, surname, email, interestings, job, universe_degree, looking_for FROM ev_users WHERE id = ?', row.admin);
+            const admin = await db.queryOne(
+              "SELECT id, name, surname, email, interestings, job, universe_degree, looking_for FROM ev_users WHERE id = ?",
+              row.admin
+            );
             row.admin = admin;
-          };
+          }
         }
         rows.push(await JSON.parse(crypto.decrypt(token)));
-        await socket.emit('events.list', rows);
+        await socket.emit("events.list", rows);
       });
     });
 
-    socket.on('events.get.id', (obj)=>{
-      db.get('SELECT * FROM ev_events WHERE id = ?', obj.id, async (err, row)=>{
-        if (row){
-          const admin = await db.queryOne('SELECT id, name, surname, email, interestings, job, universe_degree, looking_for FROM ev_users WHERE id = ?', row.admin);
-          row.admin = admin;
+    socket.on("events.get.id", (obj) => {
+      db.get(
+        "SELECT * FROM ev_events WHERE id = ?",
+        obj.id,
+        async (err, row) => {
+          if( err ) console.error(err);
+          if (row) {
+            const admin = await db.queryOne(
+              "SELECT id, name, surname, email, interestings, job, universe_degree, looking_for FROM ev_users WHERE id = ?",
+              row.admin
+            );
+            row.admin = admin;
+            row.currentUser = await JSON.parse(crypto.decrypt(obj.token));
+          }
+          
+          await socket.emit("events.one", row);
         }
-        row['currentUser'] = await JSON.parse(crypto.decrypt(obj.token));
-        await socket.emit('events.one', row);
-      });
+      );
     });
 
-    socket.on('events.create', (obj)=>{
+    socket.on("events.create", (obj) => {
       if (!obj.title || obj.title.trim() + "" === "") {
         io.emit("events.create.error", "Title should be filled");
         return;
@@ -137,23 +158,109 @@ module.exports = (io) => {
         io.emit("events.create.error", "Description should be filled");
         return;
       }
-      if (!obj.datetime || !parseInt(obj.datetime) ) {
+      if (!obj.datetime || !parseInt(obj.datetime)) {
         io.emit("events.create.error", "Date-time should be filled");
         return;
       }
 
-      db.run('INSERT INTO ev_events (title, description, admin, date) VALUES (?, ?, ?, ?)',
-      obj.title,
-      obj.description,
-      JSON.parse(crypto.decrypt(obj.token)).id,
-      obj.datetime,
-      (err)=>{
-        if (err) throw err;
-        else io.emit("events.create.success");
-      });
-
-
+      db.run(
+        "INSERT INTO ev_events (title, description, admin, date) VALUES (?, ?, ?, ?)",
+        obj.title,
+        obj.description,
+        JSON.parse(crypto.decrypt(obj.token)).id,
+        obj.datetime,
+        (err) => {
+          if (err) throw err;
+          else io.emit("events.create.success");
+        }
+      );
     });
 
+    socket.on("events.join", (obj) => {
+      db.get(
+        "SELECT subscribers FROM ev_events WHERE id = ?",
+        obj.id,
+        (err, row) => {
+          if (row) {
+            const currentUser = JSON.parse(crypto.decrypt(obj.token));
+            if (
+              !!row.subscribers &&
+              JSON.parse(row.subscribers).includes(currentUser.id)
+            ) {
+              socket.emit(
+                "events.join.error",
+                "You already have subscribed on this event"
+              );
+            } else {
+              let subscribers = [];
+              if (row.subscribers) {
+                subscribers = JSON.parse(row.subscribers);
+              }
+
+              subscribers.push(currentUser.id);
+              db.run(
+                "UPDATE ev_events SET subscribers = ? WHERE id = ?",
+                JSON.stringify(subscribers),
+                obj.id,
+                (err) => {
+                  if (err) throw err;
+                  else socket.emit("events.join.success");
+                }
+              );
+            }
+          } else {
+            socket.emit("events.join.error", "Event doesn't exists");
+          }
+        }
+      );
+    });
+
+    socket.on("events.exit", (obj) => {
+      db.get(
+        "SELECT subscribers FROM ev_events WHERE id = ?",
+        obj.id,
+        (err, row) => {
+          if (row) {
+            const currentUser = JSON.parse(crypto.decrypt(obj.token));
+            if (
+              !!row.subscribers &&
+              JSON.parse(row.subscribers).includes(currentUser.id)
+            ) {
+              let subscribers = JSON.parse(row.subscribers);
+
+              delete subscribers[subscribers.indexOf(currentUser.id)];
+              db.run(
+                "UPDATE ev_events SET subscribers = ? WHERE id = ?",
+                JSON.stringify(subscribers),
+                obj.id,
+                (err) => {
+                  if (err) throw err;
+                  else socket.emit("events.exit.success");
+                }
+              );
+            } else {
+              socket.emit(
+                "events.join.error",
+                "You haven't joined this event"
+              );
+            }
+          } else {
+            socket.emit("events.join.error", "Event doesn't exists");
+          }
+        }
+      );
+    });
+
+
+    socket.on("events.remove", (id) => {
+      db.run(
+        "DELETE FROM ev_events WHERE id = ?",
+        id,
+        (err) => {
+          if (err) throw err;
+          else socket.emit("events.remove.success");
+        }
+      );
+    });
   });
 };
